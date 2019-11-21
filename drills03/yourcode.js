479878caaf407c92e3f8f778b9bdf1586e933ecd
Christie Hui <cwsh15@gmail.com>
2019-11-20 21:28:13 -0800

    finished 1/5 drills03

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,11 +1,24 @@
 
 
-function getPath(){
-
+function getPath(array){
+  var output = array.split('/');
+  return output;
 }
 
-function getPathParts(){
-
+function getPathParts(array){
+  var parts = getPath(array);
+  console.log(parts);
+  var portPosition = parts[2].indexOf(':');
+  var host = parts[2].slice(0, portPosition);
+  var port = Number.parseInt((parts[2].slice(portPosition+1)));
+  var output = {
+    "protocol" : parts[0].slice(0, -1),
+    "host": host,
+    "port": port,
+    "path": parts[3],
+    "file": parts[4]
+  };
+  return output;
 }
 
 function getCapitalCount(){
