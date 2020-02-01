e693914ee60fc4f42096965b9dca2f24a9c15113
Christie Hui <cwsh15@gmail.com>
2020-01-31 22:40:27 -0800

    finished drills06

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,38 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement(element, classToRemove){
+  var targetElement = $(element);
+  targetElement.removeClass(classToRemove);
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement(element, classToToggle){
+  var targetElement = $(element);
+  targetElement.toggleClass(classToToggle);
 }
 
-function hideElements(  ){
-	
+function hideElements(element, removalType){
+  var targetElement = $(element);
+  if (removalType === 'hide') {
+    targetElement.hide();
+  } else if (removalType === 'removeChildren') {
+    targetElement.empty();
+  } else if (removalType === 'removeSelf') {
+    targetElement.remove();
+  }
 }
 
-function addAttributeToElement( ){
-	
+function addAttributeToElement(element, attr, value){
+  var targetElement = $(element);
+  targetElement.attr(attr, value);
 }
 
-function putPosInElement(  ){
-	
+function putPosInElement(element){
+  var targetElement = $(element);
+  var position = targetElement.position();
+  var output = {
+    'x' : position.top,
+    'y' : position.left
+  }
+  targetElement.text('x: ' + output.x + 'px' + ' ' + 'y: ' + output.y + 'px');
+  return output;
 }
-
-
